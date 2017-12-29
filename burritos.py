efbe9f77cceea104ff0996fb05ff5068ee8209d4
backup <ec2-user@ip-172-31-78-255.ec2.internal>
2017-12-29 20:00:01 +0000

    autocommit

--- burritos.py
+++ burritos.py
@@ -1,50 +1,56 @@
 #change to include proper cases for punctuation.
-print('Did you eat lunch today?')
 
-response=input().lower()
+output=print()
 
-yesList=['y','yes', 'yeah' , 'ya', 'yup', 'yes i did']
-noList=['n', 'no', 'naw', 'nah', 'nope', "no i didn't", 'no i did not']
+for item in output:
+    print(item[0].upper() + item[1:], end='. ')
 
+    print('Did you eat lunch today?')
 
-if response in yesList: #change to accept multiple versions of response
-    print('What did you eat for lunch?')
-if response in noList:
-    print('What you you like to eat?')
+    response=input().lower()
 
-foods=input()
-suffix='s'
-if foods.endswith(suffix):
-    x='many'
-else:
-    x='much'
-
-if response in yesList:
-
-    print(foods + "."' that sounds good. How ' + x+ ' ' + foods + ' did you have?')
-
-#adjust code to detect plurals and change string accordingly
-
-if response in noList:
-    print(foods + "."' that sounds good. How '+ x + ' ' + foods + ' will you have?')
-#convert to upper case at the beginning of the string and after the periods
-
-try:
-
-    number=int(input())
+    yesList=['y','yes', 'yeah' , 'ya', 'yup', 'yes i did']
+    noList=['n', 'no', 'naw', 'nah', 'nope', "no i didn't", 'no i did not']
 
 
-    if number < 5:
-        print('Are you on a diet?')
+    if response in yesList: #change to accept multiple versions of response
+        print('What did you eat for lunch?')
+    if response in noList:
+        print('What you you like to eat?')
 
-    if (number >= 5) and (number <= 19):
-        print('Slow down piggy, leave some for the rest of us')
+    foods=input()
+    suffix='s'
+    if foods.endswith(suffix):
+        x='many'
+    else:
+        x='much'
 
-    if number > 20:
-        print(str(number) + '! Are you training for an eating contest?')
+    if response in yesList:
 
-except ValueError:
-    response=str(input())
-    print(response + '. Good job!')
+        print(foods + "."' that sounds good. How ' + x+ ' ' + foods + ' did you have?')
 
-# add contingencies for string responses
+    #adjust code to detect plurals and change string accordingly
+
+    if response in noList:
+        print(foods + "."' that sounds good. How '+ x + ' ' + foods + ' will you have?')
+    #convert to upper case at the beginning of the string and after the periods
+
+    try:
+
+        number=int(input())
+
+
+        if number < 5:
+            print('Are you on a diet?')
+
+        if (number >= 5) and (number <= 19):
+            print('Slow down piggy, leave some for the rest of us')
+
+        if number > 20:
+            print(str(number) + '! Are you training for an eating contest?')
+
+    except ValueError:
+        response=str(input())
+        print(response + '. Good job!')
+
+    # add contingencies for string responses
