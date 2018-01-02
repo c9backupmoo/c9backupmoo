0783e98c2da3561b614ac9f68766ff8a43994f03
backup <ec2-user@ip-172-31-78-255.ec2.internal>
2018-01-02 18:00:01 +0000

    autocommit

--- Comma_code_2.py
+++ Comma_code_2.py
@@ -1,4 +1,9 @@
+#you are supposed to be able to use a comma (item,) keep all items on one line
+
 spam = ['apples', 'bananas', 'tofu', 'cat']
 
-for i in spam[0:-1]:
-    print(str(i)[:3] + ', ')
+#for i in spam[0:-1]:
+   # print((str(i) +', ')+('and') + str(i)[-1])
+
+for item in spam[0:-1]:
+    print(str(item+ ', ' ) + ('and'))
