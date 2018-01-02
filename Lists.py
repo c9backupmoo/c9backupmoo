be40937a8c526590d8798a552f07217461ff3053
backup <ec2-user@ip-172-31-78-255.ec2.internal>
2018-01-02 21:00:01 +0000

    autocommit

--- Lists.py
+++ Lists.py
@@ -4,11 +4,18 @@ r =input().lower()
 yesList=['y','yes']
 noList=['n','no']
 
-if r in yesList:
+for r in yesList:
     print('What would you like to call your list?')
-if r in noList:
-    print('Would you like to add to an existing list?')
 name=input()
 newList=name
 print('Input your list items for ' + newList.upper())
+
+for r in noList:
+    print('Would you like to add to an existing list?')
+
+    #if r in yesList:
+
+        #print('What list would you like to alter?')
+
+
 
