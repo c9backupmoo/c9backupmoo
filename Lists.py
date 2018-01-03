794fd92f1be78081ee6bcc853ac45faf91d9ad36
backup <ec2-user@ip-172-31-78-255.ec2.internal>
2018-01-03 00:00:01 +0000

    autocommit

--- Lists.py
+++ Lists.py
@@ -1,21 +1,39 @@
-print('Would you like to make a list?')
+def start():
+    print('Would you like to make a list?')
 
-r =input().lower()
-yesList=['y','yes']
-noList=['n','no']
+    r =input().lower()
+    yesList=['y','yes']
+    noList=['n','no']
 
-if r in yesList:
-    print('What would you like to call your list?')
-    name=input()
-    newList=name
-    print('Input your list items for ' + newList.upper())
+    if r in yesList:
+        print('What would you like to call your list?')
+        name=input()
+        newList=name
+        print('Input your list items for ' + newList.upper())
+        newList=[]
+        add=input()
+        newList.append(add) # is not adding to the list
+        for item in newList[:-1]:
+            print(newList(item))
 
-if r in noList:
-    print('Would you like to add to an existing list?')
+    if r in noList:
+        print('Would you like to add to an existing list?')
+        answer=input()
+        if answer in noList:
+            print('You are out of options.')
+            print('Would you like to start from the beginning?')
 
-    #if r in yesList:
+            option=input()
+            if option in noList:
 
-        #print('What list would you like to alter?')
+                print('Goodbye')
+                exit()
+            if option in yesList:
+                start()
+        if answer in yesList:
+            print('What list would you like to alter?')
+            listName=input()
 
 
+start()
 
