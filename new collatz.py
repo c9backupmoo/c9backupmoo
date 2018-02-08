9506633f59fdb2ebd944377fac08f56e950ed68d
backup <ec2-user@ip-172-31-78-255.ec2.internal>
2018-02-08 21:00:01 +0000

    autocommit

---
+++ new collatz.py
@@ -0,0 +1,19 @@
+def collatz:
+
+number=()
+
+    if number() %2 == 0:
+    return //2
+
+    if number() %2==1:
+    return 3* + 1
+
+    print (return)
+
+print('input number')
+
+while number()!= 1:
+    collatz
+
+    if number()==1:
+    print('Done!')
